4d2bd7f80b1edbd594f9566e9036f6335c973fe6
abdoayman20002025-ops <abdoayman20002025@gmail.com>
2026-03-08 18:05:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,18 @@ CHANNEL = "@elqanas2024"
 ADMIN_ID = 6831561700
 proxies = [
 
-{"ip":"157.173.199.2","port":"7070","username":"qanas","password":"qanas"}
+{"ip":"157.173.199.2","port":"7070","username":"qanas","password":"qanas"},{"ip":"162.245.238.81","port":"6868","username":"user646359706-1772991829","password":"9997e742b574"},
+{"ip":"222.5.99","port":"3838","username":"user12287125144-1772991646","password":"a567ab4a3c18"},
+{"ip":"5.221.133.39","port":"4479","username":"user12745686344-1772991646","password":"2ca567e31e18"},
+{"ip":"5.127.93.60","port":"4569","username":"user12322848583-1772991646","password":"68c56fb75a"},
+{"ip":"193.42.244.123","port":"2538","username":"user12127968284-1772991646","password":"e7b617bbcd"},
+{"ip":"74.81.36.31","port":"8999","username":"user341992536-1772991646","password":"cb9d8f2d52"},
+{"ip":"74.81.45.183","port":"2222","username":"user931283466-1772991646","password":"2d46a5a667"},
+{"ip":"185.127.93.60","port":"4569","username":"user327161266-1772991646","password":"53d7b21b5f"},
+{"ip":"74.81.35.2","port":"3339","username":"user12580282014-1772991646","password":"2d92f5596c"},
+{"ip":"74.81.45.183","port":"2222","username":"user12901921344-1772991646","password":"6b5a15dc9b"},
+{"ip":"147.124.205.248","port":"6999","username":"user420839945-1772991646","password":"2df533ae5a"},
+{"ip":"74.81.40.205","port":"1159","username":"user824093306-1772991646","password":"206ce9b7e9"}
 
 ]
 
